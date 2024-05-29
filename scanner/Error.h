e19689173e017012bbbb590ebdb60204f74f4d58
Samyxandz <tamaghna.official@gmail.com>
2024-05-29 09:11:31 +0530

    Good times for a change See, the luck I've had Can make a good man Turn bad So please, please, please Let me, let me, let me Let me get what I want This time

--- scanner/Error.h
+++ scanner/Error.h
@@ -3,11 +3,8 @@
 
 inline bool hadError = false;
 
-static void report(int line, std::string_view where,
-                   std::string_view message) {
-  std::cerr <<
-      "[line " << line << "] Error" << where << ": " << message <<
-      "\n";
+static void report(int line, std::string_view where,std::string_view message) {
+  std::cerr <<"[line " << line << "] Error" << where << ": " << message <<"\n";
   hadError = true;
 }
 
